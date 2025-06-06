625b81d1dcd13eb6c41f7e7d3720d29b497bca3d
Song Jihan <challengedbipolar@gmail.com>
2025-06-06 10:54:46 +0900

    remove pages, add github link button/projects, more gap between grids

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+"use strict";
+
 document.addEventListener("DOMContentLoaded", function()
 {
 	const lightbox = document.getElementById("lightbox");
@@ -36,15 +38,19 @@ document.addEventListener("DOMContentLoaded", function()
     addProject("고기산책로", "2036043", "이선귀", "/images/2036043_poster.png", "1SkvpUW8DWGda9a9NDVbM-WY-B54apZYO", "1yaI90e9Exe2U4Y_AKgWMEhL3ZmR8zhzQ", "https://github.com/sungwidard7/Project_MeatPath.git");
     addProject("Dungeon Slayer", ["2088002", "2088018", "2288020"], ["경정찬", "나정원", "윤동욱"], "", "1bV4_z3aOStQ5l7VFQKMN-zgikvWmN814", "1JxRCfvMWImg6BepkfCMHEq0MZ2P3wAWY", "");
     addProject("AirStrike", "2088005", "권현민", "/images/2088005_poster.png", "1gYbS2bnQtzbmkmBfdDAEJozZ4rw29cR1", "1nWZbS76faY57b3033qtzTj8XEhOTlq0u", "");
-    addProject("냥이의 식탁", ["2088011", "2088041"], ["김지유", "정완희"], "/images/2088011_2088041_poster.png", "", "", "");
+    addProject("냥이의 식탁", ["2088011", "2088041"], ["김지유", "정완희"], "/images/2088011_2088041_poster.png", "1x27UkB-AGs6xJZZzWPNJbBRN8OB_H0XF", "1NzCPSAvqkol2fngJhrltF1b8nSrlyiJJ", "https://dhksgml.github.io/NyangsTable/");
+	addProject("전탄발사", "2088013", "김진욱", "/images/2088013_poster.png", "1OfqibmH3xb_onV2tX82yylssf2lya-Ge", "1RflnXjKlurPO_zv8JNouNYAROBiiGftz", "")
     addProject("NIGHTFALL RUSH", "2088015", "김찬영", "/images/2088015_poster.png", "1GWb5yj9fZdFCRKwY1C8E2UDrNT8-y0S6", "1YV38NW1xTiisYbLh1k7JwoAxhd9MeUpa", "https://github.com/rlacksdud01/NightFall_Rush");
+	addProject("Farm Farm Island", "2088019", "남혁진", "/images/2088019_poster.png", "1SBWK8CRkLA9w9lUMgeLYlozJ-Xje3pl3", "1_VVimr9xz7h7MIsuOP_or9sgCdEpsLSV", "https://github.com/HykJn/Project_FFI");
     addProject("Dungeon Crawl", "2088021", "박상범", "/images/2088021_poster.png", "1M36sBrPeBFsvfu1Cwgr6pJhq8xMN9SUP", "17Ojsn0A6KPX7-MqRxMO3_2QjLqSzkDFm", "https://github.com/WeakSpoter/School-Project.git");
     addProject("BPM: BEATS PER MINUTE", "2088026", "신중훈", "/images/2088026_poster.png", "1XDD0A_VGmlwS64ekQtrkDWI4IFH_2xad", "1x_EI1FU40hfOeVjZ13UytTdrvzZHxuo3", "https://github.com/ShinJooongHooon/BPM");
+    addProject("BIG HEAD", "2088030", "윤준서", "/images/2088030_poster.png", "1ObcsYBsceudnWAl6R2cbRyj9-n6oNC-1", "1emQHqlEArlhj3s7jmxwckZKtMS6TgCxS", "");
     addProject("UNDEAD RUSH", "2088035", "장윤호", "/images/2088035_poster.png", "1x3psjjg__b4pjjl5euZ5JUnmaClZ4U-K", "1q45P_3P7yOh5aEQRhDRb_sUzglr8hAn4", "");
     addProject("비연 (PC)", "2088037", "전민성", "/images/2088037_poster.png", "1_d8Ocg1yE7t6yivrn19l65NCdVyeuqi6", "1ZPiPIUztD4HLNh34drlt3jjUnwq3awOd", "https://github.com/JMS2001K/Flying-Swallow/tree/main");
     addProject("A+", "2088044", "최문섭", "/images/2088044_poster.png", "1mWCyKG5psDCVuEiqrggu6An8O0YzlMrT", "1467uaxd7F2bA-mko-KSDPXpItabRcg7_", "");
-    addProject("Tele Connect", "2088046", "황인수", "", "125yFxSlJ4YKgvzolOVADzEn-Sh7vc-CV", "", "");
-    addProject("현대공군대결", "2188802", "최민섭", "/images/2188802_poster.png", "1BnMe8oqRYMM0sIPhoJD-D06F9l6K1990", "", "https://github.com/f1709/-/blob/main/README.md");
+    addProject("Tele Connect", "2088046", "황인수", "/images/2088046_poster.png", "125yFxSlJ4YKgvzolOVADzEn-Sh7vc-CV", "", "https://github.com/psychicmage/Project-VA-.git");
+	addProject("PROJECT-T", "2188045", "노영준", "/images/2188045_poster.jpg", "1iScayp_5OfhAaM-CLWyyPDZO59zFhVSb", "")
+    addProject("현대공군대결", "2188802", "최민섭", "/images/2188802_poster.png", "1BnMe8oqRYMM0sIPhoJD-D06F9l6K1990", "19DeC7e6FoIvQNIaG0YVDL3iObX1l6qm_", "https://github.com/f1709/-/blob/main/README.md");
     addProject("Escape from Wizard: Sun & Moon", ["2288014", "2288014"], ["김다희", "남경서"], "/images/2288014_2288014_poster.png", "1lnLCmHVJbPfftTbHTMmD1VA57B-4F9qO", "1MYGC_udgfLUIDPLhcltzay38QiEL_WjM", "");
     addProject("2Dragon -혈무-", ["2288049", "2288012"], ["박건우", "김승석"], "/images/2288049_2288012_poster.png", "1mNoHw4T-92HCpAgpDNzRAsSqUNXwKGpV", "1YkkdzfsyQvBQfBgHC-WE0xXlGkcvjeyS", "https://github.com/Nyam03/2Dragon_Capstone");
 
@@ -60,37 +66,32 @@ projects.forEach(function(proj)
 	let titleLength = proj.title.length;
 	let titleFontSize = titleLength >= 22 ? "16px" : "18px";
 
-	// githubLink가 있을 때만 <a> 태그로 감싸기
-	let linkStart = proj.githubLink
-		? `<a href="${proj.githubLink}" target="_blank" style="text-decoration: none; color: inherit;">`
-		: "";
-	let linkEnd = proj.githubLink ? `</a>` : "";
-
-	itemWrapper.innerHTML =
-		`<div class="subtitle">
-			${linkStart}
-				<div style="
-					font-weight: bold;
-					font-size: ${titleFontSize};
-					text-align: center;">
-					${proj.title}
+	itemWrapper.innerHTML = `
+				<div class="subtitle">
+					<div style="
+						font-weight: bold;
+						font-size: ${titleFontSize};
+						text-align: center;
+					">
+						${proj.title}
+					</div>
+					<div style="
+						font-size: ${nameFontSize};
+						text-align: center;
+						color: #555;
+						white-space: nowrap;
+						overflow: visible;
+						text-overflow: unset;
+					">
+						${proj.studentIds.map((id, i) => `${id} ${proj.names[i]}`).join(" | ")}
+					</div>
 				</div>
-				<div style="
-					font-size: ${nameFontSize};
-					text-align: center;
-					color: #555;
-					white-space: nowrap;
-					overflow: visible;
-					text-overflow: unset;">
-					${proj.studentIds.map((id, i) => `${id} ${proj.names[i]}`).join(" | ")}
-				</div>
-			${linkEnd}
-		</div>
-		<img class="thumbnail" src="${proj.poster}" alt="포스터">
-		<div class="buttons">
-			<button class="video-btn" data-video-id="${proj.videoId}">동영상</button>
-			<button class="download-btn" data-download-id="${proj.downloadId}">다운로드</button>
-		</div>`;
+				<img class="thumbnail" src="${proj.poster}" alt="포스터">
+				<div class="buttons">
+					<button class="video-btn" data-video-id="${proj.videoId}">동영상</button>
+					<button class="download-btn" data-download-id="${proj.downloadId}">다운로드</button>
+					${proj.githubLink ? `<button class="github-btn" data-github-link="${proj.githubLink}">Github</button>`: ``}
+				</div>`;
 
 	grid.appendChild(itemWrapper);
 });
@@ -169,7 +170,26 @@ projects.forEach(function(proj)
         });
 	});
 
-	const items = Array.from(document.querySelectorAll(".item"));
+		document.querySelectorAll(".github-btn").forEach(function(btn)
+	{
+        btn.addEventListener("click", function()
+        {
+			const githubLink = this.dataset.githubLink;
+
+            /* if (!githubLink) 버튼 표시를 안하는 방향으로 변경.
+            {
+                alert("Github 주소가 제출되지 않았습니다.");
+                return;
+            } */
+
+            let url = githubLink;
+
+            window.open(url, "_blank");
+        });
+	});
+
+	// 페이지 관련부분, 페이지를 없애고 아래로 스크롤하게 변경.
+	/* const items = Array.from(document.querySelectorAll(".item"));
 	const itemsPerPage = 8;
 	const pageCount = Math.ceil(items.length / itemsPerPage);
 	const paginationContainer = document.getElementById("pagination");
@@ -258,5 +278,5 @@ projects.forEach(function(proj)
 		}
 	}
 
-	showPage(currentPage);
+	showPage(currentPage); */
 });
